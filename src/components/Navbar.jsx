9a76225938657b33ddfc4e3f071c6d7247fea803
Jose <jrodriguez4013@gmail.com>
2023-02-08 08:45:08 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <div className='flex text-white justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
       <ul className='hidden md:flex'>
-        <li className='p-4'>Home</li>
+        <li className='p-4'>Homee</li>
         <li className='p-4'>Resources</li>
         <li className='p-4'>Company</li>
         <li className='p-4'>Home</li>
